483563953bcbebfa3fded8898051219e40ccf63f
Ishwor Khadka <khadkaishwor44@gmail.com>
2020-04-01 13:55:30 +1100

    Updates

---
+++ precache-manifest.c6da578aafa67bc6a3a074c896ff6533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0884577d03a25d3c7d81fce65a031140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f22322fb1cdbfef560",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "544288353b3b47a0a8a6",
+    "url": "/static/js/2.f468b3fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f468b3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f22322fb1cdbfef560",
+    "url": "/static/js/main.f41889c3.chunk.js"
+  },
+  {
+    "revision": "5797d46da30f2a2273be",
+    "url": "/static/js/runtime-main.3bf9f130.js"
+  }
+]);
